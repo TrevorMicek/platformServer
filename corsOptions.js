192cc6acdc1d1f0c5cb311d06684b3331a75805e
TrevorMicek <miceking@comcast.net>
2021-07-13 15:26:53 -0600

    edited files

--- corsOptions.js
+++
@@ -1,6 +0,0 @@
-var corsOptions = {
-    origin: 'http://localhost:4000/',
-    optionsSuccessStatus: 200// some legacy browsers (IE11, various SmartTVs) choke on 204
-
-}
-module.exports = corsOptions;
