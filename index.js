f70f3609fe9b7d57d577b7ea24b49478facb8e4c
TrevorMicek <miceking@comcast.net>
2021-07-13 18:54:47 -0600

    route fix

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
 app.use(cors({origin:true,credentials: true}));
 if (process.env.NODE_ENV === 'production') {
-   app.use('/', express.static('/testPage'));
+   
    app.use('/api/page', express.static(path.join(__dirname, '../server/test')));
 }
 app.get('*', (req, res) => {
