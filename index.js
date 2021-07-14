3fd9b6495a9d2261d8548c918a60e74b7f2223b9
TrevorMicek <miceking@comcast.net>
2021-07-13 21:39:37 -0600

    fix route

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json())
 app.use(cors({origin:true,credentials: true}));
 
 
-   app.use('/api/page/', express.static(path.join(__dirname, '../server/test')));
+app.use('/page/', express.static(path.join(__dirname, '../server/test')));
 
 
 app.post("/api/page/:page", function(req, res) {
