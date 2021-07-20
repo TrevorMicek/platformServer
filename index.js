62420c92a32654d937dc53eb259463ddb7c1f6c9
TrevorMicek <miceking@comcast.net>
2021-07-20 15:15:00 -0600

    cors options

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const { response } = require('express');
 
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
-//app.use(cors());
-app.options('/api/page/:page', cors({origin:true,credentials: true}))
+app.use(cors({origin:true,credentials: true}));
+app.options('/api/page/:page', cors())
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('testPage'))
@@ -29,7 +29,7 @@ app.use('/api/page/', express.static(path.join(__dirname, './test')));
    
  app.post("/api/page/:page", function(req, res) {
     res.header("Access-Control-Allow-Origin", "https://platformservice.netlify.app/"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         if (req.body.add === undefined && req.body.delete === undefined && req.body.to === undefined ) {
             res.redirect('https://platformservice.netlify.app/')
         } else {
