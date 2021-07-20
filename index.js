6bf33ea021e3be1d0c421f50aff1ef1f3eb21443
TrevorMicek <miceking@comcast.net>
2021-07-20 15:16:33 -0600

    origin

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const { response } = require('express');
 
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
-app.use(cors({origin:true,credentials: true}));
+app.use(cors({origin:"https://platformservice.netlify.app/",credentials: true}));
 app.options('/api/page/:page', cors())
 
 if (process.env.NODE_ENV === 'production') {
