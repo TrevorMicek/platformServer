b293ce0f36133cf94e4303fa7d7c09af4e734d1a
TrevorMicek <miceking@comcast.net>
2021-07-20 15:23:26 -0600

    origin fix

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const { response } = require('express');
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
 let corsOptions = {
-    origin:"https://platformservice.netlify.app/",
+    origin:"https://platformservice.netlify.app",
     credentials: true
 }
 app.use(cors(corsOptions))
