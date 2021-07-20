e701c5b2ff928ca3f2a4f3cd75e24766f118d9c6
TrevorMicek <miceking@comcast.net>
2021-07-20 15:19:53 -0600

    cors options

--- index.js
+++ index.js
@@ -14,8 +14,12 @@ const { response } = require('express');
 
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
-app.use(cors({origin:"https://platformservice.netlify.app/",credentials: true}));
-app.options('/api/page/:page', cors())
+let corsOptions = {
+    origin:"https://platformservice.netlify.app/",
+    credentials: true
+}
+app.use(cors(corsOptions))
+//app.options('/api/page/:page', cors())
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('testPage'))
