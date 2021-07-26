842a2f243e47e32fdb4bd851784481d95b03e82d
TrevorMicek <miceking@comcast.net>
2021-07-25 23:33:31 -0600

    switch delete

--- test/change.js
+++ test/change.js
@@ -68,7 +68,7 @@ fs.readFile(`${filePath}`, "utf8", function(err, data) {
                     return JSON.stringify(parseData, null, 2)
                 case switchComponent:
                     parseData.push(switchComponent[0])
-                    parseData.splice(switchComponent.indexOf(switchComponent[i]), 1)
+                    parseData.splice(switchComponent[0], 1)
                     return JSON.stringify(parseData, null, 2)
                 default:
                     console.log('error')
