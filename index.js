7bd9d03d276d13c8e9dba1dc68c1df37cf913360
TrevorMicek <miceking@comcast.net>
2021-08-20 20:36:59 -0600

    huge update with new text editor GUI + images through CDN + fixed more

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require('path');
 const bodyParser = require("body-parser");
 
 
-const firstPage = './test/first-page.json'
+const firstPage = './test/API/first-page.json'
 
 const changePage = require('./test/change');
 const { response } = require('express');
@@ -23,13 +23,13 @@ app.use(cors(corsOptions))
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('testPage'))
-    app.use('/api/page/', express.static(path.join(__dirname, './test')));
+    app.use('/api/page/', express.static(path.join(__dirname, './test/API')));
    
 }
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname))
 })
-app.use('/api/page/', express.static(path.join(__dirname, './test')));
+app.use('/api/page/', express.static(path.join(__dirname, './test/API')));
    
  app.post("/api/page/:page", function(req, res) {
             
--- test/change.js
+++ test/change.js
@@ -44,41 +44,48 @@ fs.readFile(`${filePath}`, "utf8", function(err, data) {
             let getAdd = () => {
                 for (let i=0;i<comData.length;i++) {
                     if (jsonFile.add && comData[i].component[0] === `${jsonFile.add}`) {
-                        console.log('here')
+                        
                         return comData[i]
-                    }
+                    } 
             }
              }
+             /*
              let getStyleName = () => {
                 let styleName = parseData[i].style.filter(comp => comp.hasOwnProperty(jsonFile.position))
                 console.log(styleName.hasOwnProperty(jsonFile.position))
              } 
              let newStyle = parseData.filter(comp => console.log(comp.style[jsonFile.position]))
-             console.log(newStyle)
+             */
              let newFrom = `["${jsonFile.from}"]`
              let component = parseData.filter((comp,i) => {
-                       if (JSON.stringify(comp.component) === newFrom) {
+                if (comp === undefined) {
+                    return
+                } else if (JSON.stringify(comp.component) === newFrom) {
                            return comp
                      
-                 }
+                 } 
              }           
              )
+ 
              //component[0].style[1][jsonFile.position]
-  
+             //console.log(jsonFile.switch[0] === parseData[i].component[0])
              //[0][jsonFile.position][jsonFile.property]
             //let changed = parseData[i].style.map(comp => comp[jsonFile.position])[0]
-			let changeText =  component[0].text[i][jsonFile.position] && component[0].component[0] === jsonFile.from
-		    let changeStyle = component[0].style[i][jsonFile.position] && component[0].component[0] === jsonFile.from
-            let deleteComponent = jsonFile.delete && component[0].component[0] === jsonFile.delete
+            
+            let changeStyle = component[0] && component[0].style[i][jsonFile.position] && component[0].component[0] === jsonFile.from
+			let changeText = component[0] && component[0].text[i][jsonFile.property] && component[0].component[0] === jsonFile.from
+            let deleteComponent = jsonFile.delete && parseData[i].component[0] === jsonFile.delete
             let addComponent = jsonFile.add && getAdd().component[0] === jsonFile.add
-            //let switchComponent = jsonFile.swtch && parseData[i].component[0] === jsonFile.switch
+            let switchComponent = jsonFile.switch && parseData[i].component[0] === jsonFile.switch[0]
+             console.log(jsonFile)
+            
             let replaceText = (textOrStyle) => {
-               
-               let toReplace = JSON.stringify(component[0].text[i][jsonFile.position][jsonFile.property], null, 2).replace(component[0].text[i][jsonFile.position][jsonFile.property], `${jsonFile.to}`)
-               let oldProperty = JSON.stringify(parseData[parseData.indexOf(component[0])], null, 2).replace(JSON.stringify(component[0].text[i][jsonFile.position][jsonFile.property], null, 2), toReplace)
+               console.log('text')
+               let toReplace = JSON.stringify(component[0].text[i][jsonFile.property], null, 2).replace(component[0].text[i][jsonFile.property], `${jsonFile.to}`)
+               let oldProperty = JSON.stringify(parseData[parseData.indexOf(component[0])], null, 2).replace(JSON.stringify(component[0].text[i][jsonFile.property], null, 2), toReplace)
                
               // let compToReplace = JSON.stringify(parseData[parseData.indexOf(component[0])], null, 2)
-                
+                console.log(toReplace, oldProperty)
                //console.log(data.replace(`"${JSON.stringify(component[0])}"`, `"${oldProperty}"`))
                 //console.log(oldProperty)
                 parseData.splice(parseData.indexOf(component[0]), 1, JSON.parse(oldProperty))
@@ -88,7 +95,7 @@ fs.readFile(`${filePath}`, "utf8", function(err, data) {
                
             }
             let replaceStyle = (textOrStyle) => {
-               
+               console.log('style')
                 let toReplace = JSON.stringify(component[0].style[i][jsonFile.position][jsonFile.property], null, 2).replace(component[0].style[i][jsonFile.position][jsonFile.property], `${jsonFile.to}`)
                 let oldProperty = JSON.stringify(parseData[parseData.indexOf(component[0])], null, 2).replace(JSON.stringify(component[0].style[i][jsonFile.position][jsonFile.property], null, 2), toReplace)
                 
@@ -119,19 +126,26 @@ fs.readFile(`${filePath}`, "utf8", function(err, data) {
              //.style[0][jsonFile.position][jsonFile.property]
             switch (true) {
                 case changeText:
-                  
+                    console.log('text')
                     return replaceText()
                 case changeStyle:
-                    
+                    console.log('style')
                     return replaceStyle()
-               
                 case deleteComponent:
+                    console.log('delete')
+                    jsonFile.delete === 'Holder' ? JSON.stringify(parseData, null, 2) :
                     parseData.splice(parseData.indexOf(parseData[i]), 1)
                     return JSON.stringify(parseData, null, 2)
                 case addComponent:
+                    console.log('add')
                     parseData.push(getAdd())
                     return JSON.stringify(parseData, null, 2)
-
+                case switchComponent:
+                    console.log('switch')
+                    jsonFile.add = jsonFile.switch[0]
+                    parseData.splice(parseData.indexOf(parseData[i]), 1)
+                    parseData.push(getAdd())
+                    return JSON.stringify(parseData, null, 2)
             }
             //console.log(changeText, changeStyle, deleteComponent, addComponent)
 	
